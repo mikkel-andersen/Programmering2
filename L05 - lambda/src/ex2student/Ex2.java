d2a86d00379a701d2843f89ee21db3277086156e
mikkel <mikkel.andersen2810@gmail.com>
2023-09-07 13:10:42 +0200

    L05 - Opgave 1 & 2

---
+++ L05 - lambda/src/ex2student/Ex2.java
@@ -0,0 +1,22 @@
+package ex2student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		List<Runner> runners = new ArrayList<>();
+		runners.addAll(List.of(
+				new Runner("Ib", 30),
+				new Runner("Per", 50),
+				new Runner("Ole", 27),
+				new Runner("Ulla", 40),
+				new Runner("Jens", 35),
+				new Runner("Hans", 28)));
+		System.out.println(runners);
+		runners.forEach(runner -> System.out.println("Name: " + runner.getName() + ", laptime: " + runner.getLapTime()));
+		runners.forEach(runner -> runner.getLapTime() < 30);
+
+	}
+}
