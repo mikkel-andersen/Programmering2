f7401c71982df6cdd9da0aae5f099f74b0fe36e4
mikkel <mikkel.andersen2810@gmail.com>
2023-10-02 11:21:02 +0200

    L08 - Opgave 1, 2, 2,2 & 2,3

--- L08 - Datastrukturer - List Og Map/src/opgave1/AnvendMetoderPaaHashSet.java
+++ L08 - Datastrukturer - List Og Map/src/opgave1/AnvendMetoderPaaHashSet.java
@@ -4,18 +4,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class AnvendMetoderPaaHashSet {
-
-
-    //        1) Tilføj til main erklæring og oprettelse af en mængde baseret på et HashSet.
-//                Mængden skal indeholde heltal.
-//        2) Indsæt tallene 34,12,23,45,67,34,98 i mængden.
-//        3) Udskriv indholdet af mængden.
-//        4) Indsæt tallet 23 i mængden.
-//        5) Udskriv indholdet af mængden.
-//        6) Fjern elementet 67 fra mængden.
-//        7) Udskriv indholdet af mængden.
-//        8) Undersøg om mængden indeholder elementet 23
-//        9) Udskriv hvor mange elementer der er i mængden
     public static void main(String[] args) {
         HashSet<Integer> set = new HashSet<>();
         set.addAll(Arrays.asList(34,12,23,45,67,34,98));
@@ -33,8 +21,6 @@ public class AnvendMetoderPaaHashSet {
         System.out.println(set.contains(23));
 
         System.out.println(set.size());
-
-
 
     }
 }
