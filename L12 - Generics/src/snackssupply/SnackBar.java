46d18cb72887818f0a005fe8054d8181866651dc
mikkel <mikkel.andersen2810@gmail.com>
2023-10-30 19:31:30 +0100

    L12 - Opgave 2a

--- L12 - Generics/src/snackssupply/SnackBar.java
+++ L12 - Generics/src/snackssupply/SnackBar.java
@@ -1,10 +1,17 @@
 package snackssupply;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SnackBar<E>
 {
     // TODO: Implement sortSnacks(E[] snackContainer)
 
-    public void sortSnacks(E[] snackContainer) {
-        
+    public <E extends Comparable<E>> E[] sortSnacks(E[] snackContainer) {
+        //Array to ArrayList -> collections.sort -> ArrayList to Array
+        ArrayList<E> sortering = new ArrayList<>(List.of(snackContainer));
+        Collections.sort(sortering);
+        return (E[]) sortering.toArray();
     }
 }
