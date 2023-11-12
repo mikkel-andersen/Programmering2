a2660e0e5d0dec4c8c45e98804539f6994f312b3
mikkel <mikkel.andersen2810@gmail.com>
2023-11-12 13:15:32 +0100

    L14 - rekursive opgaver

--- L14 - Rekursion2/src/Opgaver/Opgave3.java
+++ L14 - Rekursion2/src/Opgaver/Opgave3.java
@@ -1,11 +1,13 @@
 package Opgaver;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Opgave3 {
 
     public static void main(String[] args) {
-
+        ArrayList<Integer> list = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9,10));
+        System.out.println(tal(list, 7));
     }
 
 
@@ -14,6 +16,19 @@ public class Opgave3 {
     }
 
     public static boolean talHelper(ArrayList<Integer> list, int n, int low, int high, int mid) {
-        return false;
+        int mLow = low;
+        int mhigh = high;
+        int mmid = mid;
+        if (list.get(mid) == n) {
+            return true;
+        } else if (list.get(mid) > n) {
+            mhigh = mid;
+            mmid = low + (high - low) / 2;
+            return talHelper(list, n, mLow, mhigh, mmid);
+        } else {
+            mLow = mid;
+            mmid = low + (high - low) / 2;
+            return talHelper(list, n, mLow, mmid, mhigh);
+        }
     }
 }
