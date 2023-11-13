9f38f1a4eef0c1d9680829572c479caf366399da
mikkel <mikkel.andersen2810@gmail.com>
2023-11-13 08:44:07 +0100

    L14 - rekursive opgaver

--- L14 - Rekursion2/src/Helper/Hanoi.java
+++ L14 - Rekursion2/src/Helper/Hanoi.java
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  */
 package Helper;
 
@@ -7,21 +7,25 @@ package Helper;
  * @author mad
  */
 public class Hanoi {
+    static int counter = 0;
 
-	public static void flyt(int n, int fra, int til){
-		if (n==1) {
-			System.out.println("Flyt fra " +fra +" til " + til);
+    public static void flyt(int n, int fra, int til) {
+        if (n == 1) {
+            System.out.println("Flyt fra " + fra + " til " + til);
+            counter++;
 
-		}
-		else {
-			int temp = 6 - fra - til;
-			flyt(n-1,fra,temp);
-			System.out.println("Flyt fra " + fra +" til " + til);
-			flyt(n-1,temp,til);
-		}
-	}
-	public static void main(String[] args) {
-			flyt(4,1,3);
-		
-	}
+        } else {
+            int temp = 6 - fra - til;
+            flyt(n - 1, fra, temp);
+            System.out.println("Flyt fra " + fra + " til " + til);
+            flyt(n - 1, temp, til);
+            counter++;
+        }
+    }
+
+    public static void main(String[] args) {
+        flyt(10, 1, 3);
+        System.out.println(counter); //Antal træk er 2^n - 1
+
+    }
 }
